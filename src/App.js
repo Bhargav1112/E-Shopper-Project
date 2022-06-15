058b824e91be9ea1ce77d47f72e5fee247ec4b47
Bhakhar Bhargav <bhargavbhakhar1112@gmail.com>
2022-06-15 11:59:24 +0530

    initialized!

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./App.css";
+import Header from "./component/Header";
+import Footer from "./component/Footer";
+import Main from "./container/Main";
+
+function App() {
+    return (
+        <>
+            <Header />
+            <Main />
+            <Footer />
+        </>
+    );
+}
+
+export default App;
