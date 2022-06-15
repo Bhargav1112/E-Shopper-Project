5afa30bf87431acb6acbfda10c7ff10f59c2e35e
Bhakhar Bhargav <bhargavbhakhar1112@gmail.com>
2022-06-15 12:26:26 +0530

    15-06-2022

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import "./App.css";
 import Header from "./component/Header";
 import Footer from "./component/Footer";
 import Main from "./container/Main";
+import Shop from "./container/Pages/Shop";
 
 function App() {
     return (
         <>
             <Header />
             <Main />
+            {/* <Shop /> */}
             <Footer />
         </>
     );
